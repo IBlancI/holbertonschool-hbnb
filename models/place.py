18e606e73aef94d5cd0b0d89971205898798977e
jehovany04 <146716630+jehovany04@users.noreply.github.com>
2024-06-18 06:49:44 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
-# modèle/lieu.py
+from datetime import datetime
 
 class Lieu:
-    def __init__(self, id_lieu, nom, hôte):
+    def __init__(self, id_lieu, nom, hôte, latitude=None, longitude=None, prix_par_nuit=None, max_invites=None):
         if hôte is None:
             raise ValueError("Un lieu doit avoir un hôte.")
         
@@ -9,7 +9,20 @@ class Lieu:
         self.nom = nom
         self.hôte = hôte
         self.avis = []
+        self.amenities = []
+        self.latitude = latitude
+        self.longitude = longitude
+        self.prix_par_nuit = prix_par_nuit
+        self.max_invites = max_invites
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def ajouter_avis(self, avis):
         if avis not in self.avis:
             self.avis.append(avis)
+        self.updated_at = datetime.now()
+
+    def ajouter_amenity(self, amenity):
+        if amenity not in self.amenities:
+            self.amenities.append(amenity)
+        self.updated_at = datetime.now()
