4a7f5521bed33cb82228d87a86055ed4d434b911
jehovany04 <146716630+jehovany04@users.noreply.github.com>
2024-06-18 06:51:14 +0200

    Update avis.py

--- models/avis.py
+++ models/avis.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 class Avis:
-    def __init__(self, id_avis, contenu, auteur):
+    def __init__(self, id_avis, contenu, auteur, lieu):
         self.id_avis = id_avis
         self.contenu = contenu
         self.auteur = auteur
+        self.lieu = lieu
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
